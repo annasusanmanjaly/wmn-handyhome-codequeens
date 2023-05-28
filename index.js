8911b5f504266a60bc5bf84be4ea1366ede454b9
Annasusan Manjaly B <annasusanmanjaly@gmail.com>
2023-05-28 11:05:26 +0530

    backend completed

--- index.js
+++ index.js
@@ -67,3 +67,129 @@ app.post('/', function (req, res) {
     res.redirect('/');
   }
 });
+app.get('/login', function (req, res) {
+  console.log('GET /login');
+  res.sendFile(path.join(__dirname, 'public2', 'login', 'login.html'));
+});
+
+app.post('/login', function (req, res) {
+  console.log('POST /login');
+  const { email, password } = req.body;
+  console.log('Received data:', email, password);
+  if (email && password) {
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    connection.query(sql, [email], (err, result) => {
+      if (err) {
+        console.log('Error retrieving user from database:', err);
+        res.redirect('/login');
+      } else if (result.length === 1) {
+        bcrypt.compare(password, result[0].password, (err, bcryptRes) => {
+          if (err) {
+            console.log('Error comparing passwords:', err);
+            res.redirect('/login');
+          } else if (bcryptRes) {
+            req.session.loggedin = true;
+            req.session.username = result[0].username;
+            res.redirect('/home');
+          } else {
+            console.log('Invalid password');
+            res.send('<script>window.alert("Invalid password"); window.location.href = "/login";</script>');
+          }
+        });
+      } else {
+        console.log('Invalid email');
+        res.send('<script>window.alert("Invalid email"); window.location.href = "/login";</script>');
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/adress', function (req, res) {
+  console.log('GET /adress');
+  res.sendFile(path.join(__dirname, 'public2', 'adress', 'index.html'));
+});
+
+app.post('/adress', function (req, res) {
+  console.log('POST /address');
+  const { name, phone, house, road, pin, state, city } = req.body;
+  const username = req.session.username;
+
+  if (name && phone && house && road && pin && state && city) {
+    const sql = 'SELECT userid FROM users WHERE username = ?';
+    connection.query(sql, [username], (err, userids) => {
+      if (err) {
+        console.log('Error retrieving user ID from database:', err);
+        res.redirect('/adress');
+      } else if (userids.length === 1) {
+        const userid = userids[0].userid;
+        const sql2 = 'INSERT INTO adress (userid, name, phone, pin, state, city, house, road) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        connection.query(sql2, [userid, name, phone, pin, state, city, house, road], (err, result) => {
+          if (err) {
+            console.log('Error inserting address into database:', err);
+            res.redirect('/adress');
+          } else {
+            console.log('Address inserted into database:', result);
+            req.session.info = true;
+            res.redirect('/login');
+          }
+        });
+      } else {
+        console.log('Invalid user');
+        res.redirect('/adress');
+      }
+    });
+  } else {
+    console.log('Missing required fields');
+    res.redirect('/adress');
+  }
+});
+
+app.get('/home', function (req, res) {
+  console.log('GET /adress');
+  res.sendFile(path.join(__dirname, 'public2', 'Home', 'Home.html'));
+});
+app.get('/dry', function (req, res) {
+  console.log('GET /adress');
+  res.sendFile(path.join(__dirname, 'public2', 'laundry1', 'index.html'));
+});
+
+
+
+app.post('/dry', function (req, res) {
+  const { cloth, numb, dry, iron } = req.body;
+  const username = req.session.username;
+  const sql = 'SELECT userid FROM users WHERE username = ?';
+  connection.query(sql, [username], (err, userids) => {
+    if (cloth, numb, dry, iron) {
+      const sql2 = 'INSERT INTO laundry(userid, cloth, numb, dry, iron) VALUES (?, ?, ?, ?, ?)';
+      const userid = userids[0].userid;
+      connection.query(sql2, [userid, cloth, numb, dry, iron], (err, result) => {
+        if (err) {
+          console.log(err);
+          res.redirect('/dry');
+        } else {
+          console.log(result);
+          req.session.info = true;
+          res.redirect('/confirm');
+        }
+      });
+    } else {
+      res.redirect('/dry');
+    }
+  });
+});
+
+
+app.get('/confirm', function (req, res) {
+  console.log('GET /adress');
+  res.sendFile(path.join(__dirname, 'public2', 'laundry', 'index.html'));
+});
+
+
+const port = 5000;
+
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
+});
